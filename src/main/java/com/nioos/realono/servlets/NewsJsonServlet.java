1de222bd65459d6ca90693eab52672e8042832f7
hipolitojimenez <hipolitojimenez@yahoo.es>
2013-03-01 14:54:47 +0100

    apply checkstyle

--- src/main/java/com/nioos/realono/servlets/NewsJsonServlet.java
+++ src/main/java/com/nioos/realono/servlets/NewsJsonServlet.java
@@ -31,11 +31,21 @@ public class NewsJsonServlet extends HttpServlet {
     
     
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request,
-	 * HttpServletResponse response)
+	 * Called by the server (via the service method) to allow a servlet to
+	 * handle a GET request.
+	 * 
+	 * @param request an HttpServletRequest object that contains the request
+	 * the client has made of the servlet.
+	 * @param response an HttpServletResponse object that contains the
+	 * response the servlet sends to the client.
+	 * @throws IOException if an input or output error is detected when the
+	 * servlet handles the GET request.
+	 * @throws ServletException if the request for the GET could not be
+	 * handled
 	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(final HttpServletRequest request,
+				final HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 	
